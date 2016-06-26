e5d32d560968885baf101397ea7ee45b1e15f7f3
Den Odell <den.odell@akqa.com>
2016-06-26 11:59:00 +0200

    Removing old test data

--- test/index.js
+++ test/index.js
@@ -6,13 +6,13 @@ describe(`Rakuten LinkShare`, it => {
 
 	beforeEach(() => {
 		RL = new RakutenLinkShare({
-			reportingToken: 'ZW5jcnlwdGVkYToyOntzOjU6IlRva2VuIjtzOjY0OiIxNmJiNTFmZTZiYmE0MjAyODJmYzEwOThlZTliYzM4ODQ2ZTE2Zjg4MTdiMjA1NzBhNWMyZjgxZGVlMDJhMWUzIjtzOjg6IlVzZXJUeXBlIjtzOjk6IlB1Ymxpc2hlciI7fQ%3D%3D',
-			securityToken: '16bb51fe6bba420282fc1098ee9bc38846e16f8817b20570a5c2f81dee02a1e3',
-			authorisationHeader: 'Basic MzJXMjdWVGZvTGY3NUc2SHVRNDFPdTV4UWx3YTozbnBacWlPVHNVZ04zdXJadjdhM1QwaTBoWUVh',
-			username: 'denodell',
-			password: 'Unl0ck1t',
-			scope: '3325471',
-			webServicesToken: '2645f4817b96815a87c9b6affd66fe3f30e1810a13db2cf5c59c559f11f908f2',
+			reportingToken: '12345',
+			securityToken: '98765',
+			authorisationHeader: 'Basic ABCDEFG',
+			username: 'USERNAME',
+			password: 'passw0rd',
+			scope: '123123123',
+			webServicesToken: '123123123123123',
 		})
 	})
 
